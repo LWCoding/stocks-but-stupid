299f4a3fdd6e204d4d1eabf1aa36ef235864a341
Lucas Wang <lwangprivate@gmail.com>
2020-12-15 20:52:25 -0600

    Added animation to rankings page

--- public/js/rankings.js
+++ public/js/rankings.js
@@ -1,6 +1,15 @@
 window.onload = async () => {
     const winnerRes = await fetch("/richest", {method: "POST"})
     const winner = await winnerRes.json()
+    $("#first").animate({
+        opacity: 1
+    }, 500)
+    $("#second").delay(250).animate({
+        opacity: 1
+    }, 500)
+    $("#third").delay(500).animate({
+        opacity: 1
+    }, 500)
     $("#winner").text(winner.length > 0 ? winner[0].username : "Nobody")
     $("#winner-cash").text(winner.length > 0 ? winner[0].balance : "N/A")
     $("#second-place").text(winner.length > 1 ? winner[1].username : "Nobody")
